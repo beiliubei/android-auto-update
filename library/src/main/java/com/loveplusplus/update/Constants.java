190b42b0f18848f5d1cd7e00b8bb77a2b66f019d
beiliubei <beiliubei@gmail.com>
2016-06-15 16:53:12 +0800

    update json

--- library/src/main/java/com/loveplusplus/update/Constants.java
+++ library/src/main/java/com/loveplusplus/update/Constants.java
@@ -6,8 +6,8 @@ public class Constants {
 	
 	// json {"url":"http://192.168.205.33:8080/Hello/medtime_v3.0.1_Other_20150116.apk","versionCode":2,"updateMessage":"版本更新信息"}
 	//我这里服务器返回的json数据是这样的，可以根据实际情况修改下面参数的名称
-	public static final String APK_DOWNLOAD_URL = "url";
-	public static final String APK_UPDATE_CONTENT = "updateMessage";
+	public static final String APK_DOWNLOAD_URL = "updateUrl";
+	public static final String APK_UPDATE_CONTENT = "updateContent";
 	public static final String APK_VERSION_CODE = "versionCode";
 
 }
